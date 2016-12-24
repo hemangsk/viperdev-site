ee2ddd6e708cd91d859009d605999dc1fac9df31
Hemang Kumar <hemangsk@gmail.com>
2016-12-24 14:58:36 +0530

    profile

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var app = angular.module('viperdev', [])
-app.controller('Tab',  function($scope){
-	$scope.tab = 1;
-	$scope.isSet = function (stab) {
-		return $scope.tab == stab
+app.controller('Data', ['$http', function($http , $scope){
+	self = this;
+	
+	if(self.prof == null){
+
+		$http.get("https://api.github.com/orgs/viper-development/members")
+		.then(function (data) {
+			self.prof = data["data"]
+			console.log(self.prof);
+
+			angular.forEach(data["data"], function(value){
+						$http.get(value["url"])
+						.then(function (user_data) {
+							value["user_data"] = user_data["data"]
+						})
+					});
+
+
+		})
 	}
-	$scope.setTab = function (stab) {
-		$scope.tab = stab 
-	}
-})
+	
+
+
+}])
