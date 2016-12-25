57f095a0c7b8fbd5968d4bd0e5ed62c2dc4caff2
Hemang Kumar <hemangsk@gmail.com>
2016-12-25 12:46:48 +0530

    add ga

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 var app = angular.module('viperdev', [])
 app.controller('Data', ['$http', function($http , $scope){
 	self = this;
+	self.profile = [
+
+		{
+			"name" : "Mischa Krüger",
+			"id" : "@Makman2",
+			"picture" : "",
+			"bio": ""
+		},
 	
+		{
+			"name" : "Lasse Schuirmann",
+			"id" : "@sils",
+			"picture" : "",
+			"bio": ""
+		},
+
+		
+
+		{
+			"name" : "",
+			"id" : "@sims1253",
+			"picture" : "",
+			"bio": ""
+		},
+
+		{
+			"name" : "Hemang Kumar",
+			"id" : "@hemangsk",
+			"picture": "",
+			"bio" : ""
+		}
+	]
 	if(self.prof == null){
 
 		$http.get("https://api.github.com/orgs/viper-development/members")
