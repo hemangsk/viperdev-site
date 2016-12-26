c030999e4304e12c5a1531f05d57abb61fe4f5c5
Hemang Kumar <hemangsk@gmail.com>
2016-12-26 19:22:05 +0530

    app.js: Add plans in json
    Fixes #21

--- app.js
+++ app.js
@@ -39,23 +39,44 @@ app.controller('Data', ['$http', function($http , $scope){
                         "bio" : "Business expert. Founder of GitMate and solver of the most complicated business problems."
                 }
 	]
-	if(self.prof == null){
-
-		$http.get("https://api.github.com/orgs/viper-development/members")
-		.then(function (data) {
-			self.prof = data["data"]
-			console.log(self.prof);
-
-			angular.forEach(data["data"], function(value){
-						$http.get(value["url"])
-						.then(function (user_data) {
-							value["user_data"] = user_data["data"]
-						})
-					});
+	
 
 
-		})
-	}
+
+	self.plans = [
+		{
+			"title" : "Plan1",
+			"features" : [
+				"feature1",
+				"feature2",
+				"feature3"
+			]	
+		},
+		{
+			"title" : "Plan2",
+			"features" : [
+				"feature1",
+				"feature2",
+				"feature3"
+			]	
+		},
+		{
+			"title" : "Plan3",
+			"features" : [
+				"feature1",
+				"feature2",
+				"feature3"
+			]	
+		},
+		{
+			"title" : "Plan4",
+			"features" : [
+				"feature1",
+				"feature2",
+				"feature3"
+			]	
+}
+	]
 	
 
 
